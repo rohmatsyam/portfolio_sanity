7372258f8c7dd4833e48a1033d09917fd4898e61
rohmat_syam <rohmatsyam@yahoo.com>
2022-09-28 13:06:31 +0700

    HERO || complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import profilePic from "../public/img/rohmat.png";
+import Link from "next/link";
 
 type Props = {};
 
@@ -15,12 +18,34 @@ const Hero = (props: Props) => {
     delaySpeed: 2000,
   });
   return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
+    <div className="flex flex-col items-center justify-center h-screen space-y-4 overflow-hidden text-center">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor />
-      </h1>
+      <div className="relative object-cover w-32 h-32 mx-auto rounded-full">
+        <Image src={profilePic} alt="photo" layout="fill" />
+      </div>
+      <div className="z-20">
+        <h2 className="pb-2 text-sm tracking-[10px] text-gray-500 uppercase">
+          Web Developer
+        </h2>
+        <h1 className="px-10 text-2xl font-semibold lg:text-3xl">
+          <span className="mr-3">{text}</span>
+          <Cursor />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#project">
+            <button className="heroButton">Project</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
